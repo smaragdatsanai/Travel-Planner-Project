af7fce2d3d8c37ab7d83d115f368047a5a6ce3ff
Smaragda <smaragdatsanai@gmail.com>
2023-04-05 21:53:09 +0300

    user main initiallization

---
+++ App/Database/Database.sql
@@ -0,0 +1,59 @@
+-- Create a table for storing website user information
+CREATE TABLE users (
+  user_id INTEGER PRIMARY KEY,
+  username TEXT,
+  email TEXT,
+  password TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create a table for storing travel plan information
+CREATE TABLE plans (
+  plan_id INTEGER PRIMARY KEY,
+  user_id INTEGER,
+  plan_name TEXT,
+  destination TEXT,
+  start_date DATE,
+  end_date DATE,
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+-- Create a table for storing flight information
+CREATE TABLE flights (
+  flight_id INTEGER PRIMARY KEY,
+  plan_id INTEGER,
+  airline TEXT,
+  flight_number TEXT,
+  departure_date DATE,
+  departure_time TIME,
+  arrival_date DATE,
+  arrival_time TIME,
+  departure_airport TEXT,
+  arrival_airport TEXT,
+  FOREIGN KEY (plan_id) REFERENCES plans(plan_id)
+);
+
+-- Create a table for storing accommodation information
+CREATE TABLE accommodations (
+  accommodation_id INTEGER PRIMARY KEY,
+  plan_id INTEGER,
+  name TEXT,
+  check_in_date DATE,
+  check_in_time TIME,
+  check_out_date DATE,
+  check_out_time TIME,
+  location TEXT,
+  FOREIGN KEY (plan_id) REFERENCES plans(plan_id)
+);
+
+-- Create a table for storing activity information
+CREATE TABLE activities (
+  activity_id INTEGER PRIMARY KEY,
+  plan_id INTEGER,
+  name TEXT,
+  date DATE,
+  start_time TIME,
+  end_time TIME,
+  location TEXT,
+  FOREIGN KEY (plan_id) REFERENCES plans(plan_id)
+);
