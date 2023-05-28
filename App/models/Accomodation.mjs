839c73913f4f94fa22dcb2e2c62ca47c8a5d25d0
NikitasPapag <127061290+NikitasPapag@users.noreply.github.com>
2023-05-28 23:52:03 +0300

    Add files via upload

---
+++ App/models/Accomodation.mjs
@@ -0,0 +1,39 @@
+class Accomodation {
+
+    constructor(accomodation_id, plan_id, name, check_in_date, check_in_time, check_out_date, check_out_time, location) {
+        this.accomodation_id = accomodation_id;
+        this.plan_id = plan_id;
+        this.name = name;
+        this.check_in_date = check_in_date;
+        this.check_in_time = check_in_time;
+        this.check_out_date = check_out_date;
+        this.check_out_time = check_out_time;
+        this.location = location;
+    }
+
+    
+    
+    checkForUpcomingReservations() {
+
+    }
+    
+    manageReservations() {
+
+    }
+    
+    validateReservationData() {
+
+    }
+    
+    calculateDataChanges() {
+
+    } 
+
+    calculateNewDataSum() {
+
+    }
+
+    updateDatabase() {
+        
+    }
+}
---
+++ App/models/Budget.mjs
@@ -0,0 +1,48 @@
+class Budget {
+    
+    constructor(expense, state) {
+        this.expense = expense;
+        this.state = state;
+    }
+
+    budgetManagement() {
+
+    }
+    
+    budgetAnalysis() {
+
+    }
+    
+    analyzeExpenses() { //if budget sufficient
+
+    }
+    
+    budgetManagement() {
+
+    }
+    
+    budgetLow() {   //save request
+
+    }
+    
+    budgetDestribution() {
+
+    }
+    
+    addExpenses() {
+
+    }
+    
+    analyzeBudgetState() {
+
+    }
+    
+    
+    provideSavingOptions() {
+
+    }
+    
+    updateDatabase() {
+        
+    }
+}
