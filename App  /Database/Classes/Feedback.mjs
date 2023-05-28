ab036a83e9cee5ff9bb46079e1159300c19f3c2a
NikitasPapag <127061290+NikitasPapag@users.noreply.github.com>
2023-05-28 22:39:34 +0300

    Feedback.mjs

--- App  /Database/Classes/Feedback.mjs
+++ App  /Database/Classes/Feedback.mjs
@@ -1,6 +1,11 @@
 class Feedback {
 
-    this.plan_id = plan_id;
+    constructor(username, user_id, plan_name, plan_id) {
+        this.username = username;
+        this.user_id = user_id;
+        this.plan_name = plan_name;
+        this.plan_id = plan_id;
+    }
 
     updateDatabase() {
         
