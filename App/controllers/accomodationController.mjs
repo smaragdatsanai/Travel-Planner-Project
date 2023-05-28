370bb975f6eee8eb8e21fb7943bc0859f9c68d11
NikitasPapag <127061290+NikitasPapag@users.noreply.github.com>
2023-05-28 23:50:28 +0300

    Add files via upload

---
+++ App/controllers/accomodationController.mjs
@@ -0,0 +1,24 @@
+import * as accomodation from '../models/Accomodation.mjs'
+
+class AccommodationController {
+
+    static async findAccommodation(req, res) {
+        try {
+
+        }catch(error){
+            next(error)
+        }
+    }
+
+    static async checkAccommodationAvailability(req, res) {
+        try {
+
+        }catch(error){
+            next(error)
+        }
+    }
+
+    
+}
+
+export {AccomodationController}
