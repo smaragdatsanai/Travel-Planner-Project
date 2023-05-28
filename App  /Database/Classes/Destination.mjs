5dc9999eac34698771280041e8af726ce0754755
NikitasPapag <127061290+NikitasPapag@users.noreply.github.com>
2023-05-28 22:35:56 +0300

    Destination.mjs

---
+++ App  /Database/Classes/Destination.mjs
@@ -0,0 +1,32 @@
+class Destination {
+    
+    constructor(plan_id, location) {
+        this.plan_id = plan_id;
+        this.location = location;
+        }
+
+    checkForUpcomingReservations() {
+
+    }
+
+    
+    manageReservations() {
+
+    }
+    
+    validateReservationData() {
+
+    }
+    
+    calculateDataChanges() {
+
+    } 
+
+    calculateNewDataSum() {
+
+    }
+
+    updateDatabase() {
+
+    }
+}
