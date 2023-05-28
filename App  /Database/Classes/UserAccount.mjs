1e8e6d2b28590ed27795ebe964d9c46247a28cf0
NikitasPapag <127061290+NikitasPapag@users.noreply.github.com>
2023-05-28 22:30:40 +0300

    UserAccount.mjs

---
+++ App  /Database/Classes/UserAccount.mjs
@@ -0,0 +1,26 @@
+class UserAccount {
+
+    constructor(username, user_id, email, password, created_at) {
+        this.username = username;
+        this.user_id = user_id;
+        this.email = email;
+        this.password = password;
+        this.created_at = created_at;
+    }
+
+    createUserAccount() {
+
+    }
+    
+    loginAsGuest() {
+        
+    }
+    
+    userLoginAuthentication() {
+
+    }
+    
+    updateDatabase() {
+        
+    }
+}
