56a3d9720883065653a9cc8e74af3a880af8e92c
NikitasPapag <127061290+NikitasPapag@users.noreply.github.com>
2023-05-28 22:26:28 +0300

    User.mjs

---
+++ App  /Database/Classes/User.mjs
@@ -0,0 +1,62 @@
+class User {
+    
+    constructor(username, user_id, email, password, created_at) {
+        this.username = username;
+        this.user_id = user_id;
+        this.email = email;
+        this.password = password;
+        this.created_at = created_at;
+    }
+
+    hasUserAccount() {
+
+    }
+
+    
+    balanceNotification() { //if budget balance low
+
+    }
+    
+    saveReport() {
+
+    }
+    
+    requestBudgetDestribution() {
+
+    }
+    
+    requestExpenses() {
+
+    }
+    
+    buildPlan() {
+
+    }
+    
+    requestPlanData() {
+
+    }
+    
+    requestFromSuggestions() {
+
+    }       
+    
+    setPlanVisibility() {
+
+    }
+
+    requestVisibility() {
+
+    }
+    
+    requestReservationChanges() {
+
+    }
+
+    search() {
+        
+    }
+    updateDatabase() {
+        
+    }
+}
