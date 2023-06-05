a181047ecc245a4cd47e48d2b609aeb1ce2869d7
NikitasPapag <127061290+NikitasPapag@users.noreply.github.com>
2023-06-06 01:08:36 +0300

    Update Feedback.mjs

--- App/models/Feedback.mjs
+++ App/models/Feedback.mjs
@@ -1,13 +1,38 @@
 class Feedback {
 
-    constructor(username, user_id, plan_name, plan_id) {
+    constructor(username, user_id, plan_name, plan_id, stars) {
         this.username = username;
         this.user_id = user_id;
         this.plan_name = plan_name;
         this.plan_id = plan_id;
+        this.stars = stars;
+    }
+
+    loginAsGuest() {
+        
+    }
+
+    userLoginAuthentication() {
+
+    }
+
+    createReview() {
+
+    }
+
+    checkReviewRules() {
+
     }
 
     updateDatabase() {
         
+    }
+
+    choosePlan() {
+
+    }
+
+    createComplaintForm() {
+
     }
 }
