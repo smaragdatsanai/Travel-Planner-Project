8615704b4727768a6d8c951baa2d2118b071a1dd
Smaragda <smaragdatsanai@gmail.com>
2023-06-08 18:12:47 +0300

    .

---
+++ App/models/db/dbConfig.mjs
@@ -0,0 +1,36 @@
+//Sequelize
+// code connecting  Node.js application to the PostgreSQL database
+import {Sequelize} from 'sequelize';
+
+const sequelize = new Sequelize({
+    host:process.env.PGHOST||'localhost',
+    port:process.env.PGPORT,
+    dialect:'postgres',
+    username:process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database:process.env.PGDATABASE,
+    logging: false,
+    define:{
+        timestamps:false,
+        freezeTableName: true
+    },
+    
+});
+
+  
+
+
+
+const testDbConnection = async () => {
+      try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+      } catch (error) {
+        console.error("Unable to connect to the database:", error);
+      }
+};
+
+
+
+testDbConnection()
+export {sequelize}
--- App/routes/routes.mjs
+++ App/routes/routes.mjs
@@ -1,1 +1,18 @@
 
+import express from 'express'
+const router = express.Router()
+
+import * as Validator from '../validator/validation.mjs'
+
+
+import dotenv from 'dotenv'
+if (process.env.NODE_ENV !== 'production') {
+    console.log('loading .env')
+    dotenv.config();
+}
+
+import multer from 'multer';
+
+const upload = multer({ dest: 'public/uploads/' });
+
+export default router;
