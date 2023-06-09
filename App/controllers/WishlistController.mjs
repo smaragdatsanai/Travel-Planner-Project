a89653983e99ad128aa3f42afdf6d524755b9510
Smaragda <smaragdatsanai@gmail.com>
2023-06-09 22:56:51 +0300

    .

---
+++ App/controllers/WishlistController.mjs
@@ -0,0 +1,24 @@
+import * as Wishlist from '../models/Wishlist.mjs'
+
+class WishlistController {
+
+    static async getFavourites(req, res) {
+        try {
+
+        }catch(error){
+            next(error)
+        }
+    }
+
+    static async getUserPlans(req, res) {
+        try {
+
+        }catch(error){
+            next(error)
+        }
+    }
+
+    
+}
+
+export {WishlistController}
