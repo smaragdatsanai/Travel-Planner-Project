1a2cbe966901e7c8b8abe789e55062fbe353618e
NikitasPapag <127061290+NikitasPapag@users.noreply.github.com>
2023-06-11 20:04:49 +0300

    top destination

--- App/models/TravelPlan.mjs
+++ App/models/TravelPlan.mjs
@@ -1,3 +1,5 @@
+import { Destination } from './Destination.mjs';
+
 class TravelPlan {
 // , user_id, plan_name, destination, start_date, end_date
     constructor(plan_id) {
@@ -51,8 +53,44 @@ class TravelPlan {
     }
 
     chooseTopDestination() {
+    // Retrieve the destinations of the travel plan
+    const destinations = this.getDestinations();
 
-    }
+    // Calculate the popularity of each destination based on user feedback
+    const popularityMap = {};
+    destinations.forEach((destination) => {
+      const feedbacks = destination.getFeedbacks();
+      const totalFeedbacks = feedbacks.length;
+      const totalRating = feedbacks.reduce((sum, feedback) => sum + feedback.rating, 0);
+      const averageRating = totalRating / totalFeedbacks;
+      popularityMap[destination.name] = averageRating;
+    });
+
+    // Sort the destinations by popularity in descending order
+    const sortedDestinations = Object.entries(popularityMap).sort(
+      (a, b) => b[1] - a[1]
+    );
+
+    // Return the top destinations
+    const topDestinations = sortedDestinations.map(([destinationName]) => destinationName);
+    return topDestinations;
+  }
+
+  getDestinations() {
+    // Retrieve the destinations of the travel plan from the database
+    // Example implementation, replace with your actual logic
+    const destinations = Destination.findAll({
+      where: {
+        travelPlanId: this.plan_id,
+      },
+    });
+
+    return destinations;
+     }
+   }
+}
+export { TravelPlan };
+   
 
     configureDetails() {
 
