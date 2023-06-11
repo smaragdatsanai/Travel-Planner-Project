971a22191039bebe7dbe5da002cb70441de044fa
NikitasPapag <127061290+NikitasPapag@users.noreply.github.com>
2023-06-11 20:13:57 +0300

    Update TravelPlan.mjs

--- App/models/TravelPlan.mjs
+++ App/models/TravelPlan.mjs
@@ -10,7 +10,7 @@ class TravelPlan {
         this.plan_id = plan_id;
         // this.user_id = user_id;
         // this.plan_name = plan_name;
-        // this.destination = destination;
+        this.destination = destination;
         // this.start_date = start_date;
         // this.end_date = end_date;
     }
